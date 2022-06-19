6b67818c9fdf58c95de3f590669d2a394a094777
Mason Hill <masonhillsoftware@gmail.com>
2022-06-19 00:27:44 -0600

    Solved pot

---
+++ Pot/main.py
@@ -0,0 +1,13 @@
+from tkinter import Y
+
+
+x = int(input())
+
+# Read inputs and calc total. Read last index, then pow the number excluding the last digit
+total = 0
+for i in range(x):
+    y = input()
+    total += pow(int(y[:len(y) - 1]), int(y[len(y) - 1]))
+
+# Output
+print(total)
